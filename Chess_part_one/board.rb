e7cd0d2fb1c9d24098d5e0fe31f09e7fc380182c
Kevin Liu <kliu33@buffalo.edu>
2022-11-30 12:18:59 -0500

    finish slidable

--- Chess_part_one/board.rb
+++ Chess_part_one/board.rb
@@ -15,18 +15,25 @@ class Board
         @grid[pos[0]][pos[1]] = val # if @grid[pos] == :_
     end
 
-    def move_piece(start_pos, end_pos)
+    def move_piece(color, start_pos, end_pos)
         if self[start_pos].is_a?(NullPiece)
             raise "no piece here"
+        end
+
+        if color != self[start_pos].team
+            raise "not your piece"
         end
 
         if (end_pos[0] < 0 || end_pos[0] > 7) || (end_pos[1] < 0 || end_pos[1] > 7)
             raise "out of bounds"
         end
 
+
+
         current = self[start_pos]
-        self[start_pos] = :_
+        self[start_pos] = NullPiece.new(self, start_pos, nil)
         self[end_pos] = current
+        current.pos = end_pos
     end
 
     
@@ -47,41 +54,42 @@ class Board
 
     def start_board
         # Rook starting pos
-        self[[0,0]] = Rook.new(:R, [0,0], false)
-        self[[0,7]] = Rook.new(:R, [0,7], false)
-        self[[7,0]] = Rook.new(:R, [7,0], true)
-        self[[7,7]] = Rook.new(:R, [7,7], true)
+        rook1 = Rook.new(self, [0,0], :black)
+        self[[0,0]] = rook1
+        self[[0,7]] = Rook.new(self, [0,7], :black)
+        self[[7,0]] = Rook.new(self, [7,0], :white)
+        self[[7,7]] = Rook.new(self, [7,7], :white)
 
         # Knight starting pos
-        self[[0,1]] = Knight.new(:N,[0,1], false)
-        self[[0,6]] = Knight.new(:N,[0,6], false)
-        self[[7,1]] = Knight.new(:N,[7,1], true)
-        self[[7,6]] = Knight.new(:N,[7,6], true)
+        self[[0,1]] = Knight.new(self,[0,1], :black)
+        self[[0,6]] = Knight.new(self,[0,6], :black)
+        self[[7,1]] = Knight.new(self,[7,1], :white)
+        self[[7,6]] = Knight.new(self,[7,6], :white)
 
         # Bishop starting pos
-        self[[0,2]] = Bishop.new(:B,[0,2], false)
-        self[[0,5]] = Bishop.new(:B,[0,5], false)
-        self[[7,2]] = Bishop.new(:B,[7,2], true)
-        self[[7,5]] = Bishop.new(:B,[7,5], true)
+        self[[0,2]] = Bishop.new(self,[0,2], :black)
+        self[[0,5]] = Bishop.new(self,[0,5], :black)
+        self[[7,2]] = Bishop.new(self,[7,2], :white)
+        self[[7,5]] = Bishop.new(self,[7,5], :white)
 
         # Queen starting pos
-        self[[0,3]] = Queen.new(:Q,[0,3], false)
-        self[[7,3]] = Queen.new(:Q,[7,3], true)
+        self[[0,3]] = Queen.new(self,[0,3], :black)
+        self[[7,3]] = Queen.new(self,[7,3], :white)
 
         # King starting pos
-        self[[0,4]] = King.new(:K,[0,4], false)
-        self[[7,4]] = King.new(:K,[7,4], true)
+        self[[0,4]] = King.new(self,[0,4], :black)
+        self[[7,4]] = King.new(self,[7,4], :white)
 
         #Pawns starting pos
-        (0..7).each {|col| self[[1,col]] = Pawn.new(:P,[1,col], false)}
-        (0..7).each {|col| self[[6,col]] = Pawn.new(:P,[6,col], true)}
+        (0..7).each {|col| self[[1,col]] = Pawn.new(self,[1,col], :black)}
+        (0..7).each {|col| self[[6,col]] = Pawn.new(self,[6,col], :white)}
         
         i = 0
         while i < @grid.length
             j = 0
             while j < @grid.length
                 if self[[i,j]] == :_ 
-                    self[[i,j]] = NullPiece.new(:_,[i,j], nil)
+                    self[[i,j]] = NullPiece.new(self,[i,j], nil)
                 end
                 j += 1
             end
@@ -94,5 +102,10 @@ end
 
 b = Board.new
 b.start_board
-b.move_piece([1,0],[2,0])
+b.render
+b.move_piece(:black, [1,3], [5,7])
+b.move_piece(:black, [0,3], [2,3])
+b.move_piece(:black, [2,3], [6,7])
+queen = b[[6,7]]
+p queen.valid_moves
 b.render
--- Chess_part_one/piece.rb
+++ Chess_part_one/piece.rb
@@ -1,65 +1,117 @@
 # require_relative "board.rb"
+require_relative "slidable"
 
 class Piece 
 
-    attr_reader :name
-    def initialize(name, pos, team)
-        @name = name
+    attr_reader :name, :team, :pos
+    def initialize(board, pos, team)
+        @board = board
         @pos = pos
         @team = team
+        @symbol = nil
+        @move_dir = nil
+    end
+
+    def empty?
+        self.is_a?(NullPiece)
+    end
+
+    def pos=(new_pos)
+        @pos = new_pos
+    end
+
+    def valid_moves
+        
+    end
+
+
+    def name
+        @symbol
     end
 end
 
 class Sliding_pieces < Piece
-    def initalize(name, pos, team)
+    include Slidable
+    def initalize(board, pos, team)
         super
+    end
+
+    def valid_moves
+        possible_moves = moves(pos, @move_dir)
+        select_moves = []
+        possible_moves.each do |dir, arr|
+            arr.each do |xy|
+                if @board[xy].is_a?(NullPiece)
+                    select_moves << xy
+                elsif @board[xy].team == self.team
+                    break
+                else
+                    select_moves << xy
+                    break
+                end
+            end
+        end
+        select_moves
     end
 end
 
 class Stepping_pieces < Piece
-    def initalize(name, pos, team)
+    def initalize(board, pos, team)
         super
     end
 end
 
 class Queen < Sliding_pieces
-    def initialize(name, pos, team)
+    def initialize(board, pos, team)
         super
+        @symbol = :Q
+        @move_dir = :both
     end
 end
 
 class Rook < Sliding_pieces
-    def initialize(name, pos, team)
+    def initialize(board, pos, team)
         super
+        @symbol = :R
+        @move_dir = :hor
     end
+
+    
 end
 
 class Knight < Stepping_pieces
-    def initialize(name, pos, team)
+    def initialize(board, pos, team)
         super
+        @symbol = :N
     end
 end
 
 class Bishop < Sliding_pieces
-    def initialize(name, pos, team)
+    def initialize(board, pos, team)
         super
+        @symbol = :B
+        @move_dir = :diag
     end
 end
 
 class King < Stepping_pieces
-    def initialize(name, pos, team)
+    def initialize(board, pos, team)
         super
+        @symbol = :K
     end
 end
 
 class Pawn < Piece
-    def initialize(name, pos, team)
+    def initialize(board, pos, team)
         super
+        @symbol = :P
     end
 end
 
 class NullPiece < Piece
-    def initialize(name, pos, team)
+    def initialize(board, pos, team)
         super
+        @symbol = :_
     end
 end
+
